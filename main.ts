9c271e07a3106186b2e11828b027a7b874c8c842
Trol604 <142462077+Trol604@users.noreply.github.com>
2023-08-16 21:27:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ input.onButtonPressed(Button.B, function () {
     Bird.change(LedSpriteProperty.Y, 1)
 })
 let EmptyObstacle = 0
-let ticks = 0
 let Bird: game.LedSprite = null
+let ticks = 0
 let obstacles: game.LedSprite[] = []
 Bird = game.createSprite(0, 2)
 Bird.set(LedSpriteProperty.Blink, 300)
@@ -31,5 +31,5 @@ basic.forever(function () {
         }
     }
     ticks += 1
-    basic.pause(1000)
+    basic.pause(500)
 })
