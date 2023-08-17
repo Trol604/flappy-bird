9e0249142ac523d4bf7500007de50e42cec1a190
Trol604 <142462077+Trol604@users.noreply.github.com>
2023-08-16 20:53:23 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+input.onButtonPressed(Button.A, function () {
+    Bird.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    Bird.change(LedSpriteProperty.Y, 1)
+})
+let EmptyObstacle = 0
+let ticks = 0
+let Bird: game.LedSprite = null
+let obstacles: game.LedSprite[] = []
+Bird = game.createSprite(0, 2)
+Bird.set(LedSpriteProperty.Blink, 300)
+basic.forever(function () {
+    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+        obstacles.removeAt(0).delete()
+    }
+    for (let obstacle of obstacles) {
+        obstacle.change(LedSpriteProperty.X, -1)
+    }
+    if (ticks % 3 == 0) {
+        EmptyObstacle = randint(0, 4)
+        for (let index = 0; index <= 4; index++) {
+            if (index != EmptyObstacle) {
+                obstacles.push(game.createSprite(4, index))
+            }
+        }
+    }
+    for (let obstacle of obstacles) {
+        if (obstacle.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && obstacle.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
+            game.gameOver()
+        }
+    }
+    ticks += 1
+    basic.pause(1000)
+})
